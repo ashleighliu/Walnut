abea4e31255eca58cb01efebb4f05cdea2926d27
jsdandurand <114008167+jsdandurand@users.noreply.github.com>
2022-11-24 13:49:07 -0500

    Fixed Bugs

--- app/src/main/java/com/example/b07_project/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/b07_project/ui/home/HomeViewModel.java
@@ -8,9 +8,9 @@ public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
+
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
