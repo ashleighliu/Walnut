ab3b5470ccb71df36b31ec91f13685156b63c5b1
ashleighliu <ashleigh9302@gmail.com>
2022-11-25 19:49:06 -0500

    Added bottom navigation bar

--- app/src/main/java/com/example/b07_project/MainActivity.java
+++ app/src/main/java/com/example/b07_project/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.b07_project;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,14 +20,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.b07_project.databinding.ActivityMainBinding;
 
+import navbar_fragments.EditCoursesFragment;
+import navbar_fragments.HomeFragment;
+import navbar_fragments.SearchFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    EditFragment = new EditFragment();
+    SearchFragment = new SearchFragment();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl_wrapper, homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fl_wrapper, homeFragment).commit();
+                        return true;
+                    case R.id.search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fl_wrapper, searchFragment).commit();
+                        return true;
+                    case R.id.edit:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fl_wrapper, editFragment).commit();
+                        return true;
+
+                }
+                return false;
+            }
+        });
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
