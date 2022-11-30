8e790dc8a5a7cb998851e85a95bdd048d3f6122b
ashleighliu <ashleigh9302@gmail.com>
2022-11-29 20:28:08 -0500

    Added changes to login and UI

--- app/src/main/java/com/example/b07_project/StudentLanding.java
+++ app/src/main/java/com/example/b07_project/StudentLanding.java
@@ -5,76 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.MenuItem;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.widget.TextView;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.core.view.GravityCompat;
+import android.content.Intent;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class StudentLanding extends AppCompatActivity {
-    public DrawerLayout drawerLayout;
-    public ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout mDrawer;
+    //private Toolbar toolbar;
+    private NavigationView nvDrawer;
 
-    String uID;
-    String email;
-    String password;
-    DatabaseReference fire;
-    DatabaseReference user;
-    StudentAccount student;
+    // Make sure to be using androidx.appcompat.app.ActionBarDrawerToggle version.
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_landing);
+        setContentView(R.layout.activity_main);
 
+        // Set a Toolbar to replace the ActionBar.
+        //toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-
-        // drawer layout instance to toggle the menu icon to open
-        // drawer and back button to close drawer
-        drawerLayout = findViewById(R.id.my_drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
-
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-
-        // to make the Navigation drawer icon always appear on the action bar
+        // This will display an Up icon (<-), we will replace it with hamburger later
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //Retrieving account info from SharedPreferences
-        SharedPreferences p = getSharedPreferences("myPreferences", Context.MODE_PRIVATE);
-        uID = p.getString("uID", "N/A");
-        email = p.getString("email", "N/A");
-        password = p.getString("password", "N/A");
-
-        //Probably will be needed for updating the student account data (eg. adding courses, adding academic history);
-        fire = FirebaseDatabase.getInstance().getReference();
-        user = fire.child("Students").child(uID);
-
-        //Creating student object
-        student = new StudentAccount(email, password, uID);
-
-        //Default just displaying the email
-        TextView text = (TextView)findViewById(R.id.welcomeStudent);
-        text.setText("Welcome Student:" + email);
+        // Find our drawer view
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
-    // override the onOptionsItemSelected()
-    // function to implement
-    // the item click listener callback
-    // to open and close the navigation
-    // drawer when the icon is clicked
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
