b999d2f47f7904488be75e198c77599942f0fd7b
jsdandurand <114008167+jsdandurand@users.noreply.github.com>
2022-11-29 20:58:50 -0500

    Updated Course.java to use ArrayLists rather than Hashmaps for fields offeringSessions and prereqs

--- app/src/main/java/com/example/b07_project/Course.java
+++ app/src/main/java/com/example/b07_project/Course.java
@@ -1,6 +1,7 @@
 package com.example.b07_project;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -8,21 +9,21 @@ public class Course {
 
     private String courseName;
     private String courseCode;
-    private HashMap<String, String> offeringSessions;
-    private HashMap<String, String> prereqs;
+    private ArrayList<String> offeringSessions;
+    private ArrayList<String> prereqs;
 
     public Course(String courseName, String courseCode, String offeringSessions, String prereqs){
         this.courseName = courseName;
         this.courseCode = courseCode;
         String[] temp1 = offeringSessions.split(",");
-        this.offeringSessions = new HashMap<>();
-        this.prereqs = new HashMap<>();
+        this.offeringSessions = new ArrayList<String>();
+        this.prereqs = new ArrayList<String>();
         for (int i=0;i<temp1.length;i++) {
-            this.offeringSessions.put("OfferingSession " + i, temp1[i].trim());
+            this.offeringSessions.add(temp1[i].trim());
         }
         String[] temp2 = prereqs.split(",");
         for (int i=0;i<temp2.length;i++) {
-            this.prereqs.put("Prerequisite " + i, temp2[i].trim());
+            this.prereqs.add(temp2[i].trim());
         }
     }
 
@@ -35,11 +36,11 @@ public class Course {
         return courseCode;
     }
 
-    public HashMap<String, String> getOfferingSessions() {
+    public ArrayList<String> getOfferingSessions() {
         return offeringSessions;
     }
 
-    public HashMap<String, String> getPrereqs() {
+    public ArrayList<String> getPrereqs() {
         return prereqs;
     }
 
@@ -47,11 +48,11 @@ public class Course {
         this.courseCode = courseCode;
     }
 
-    public void setOfferingSessions(HashMap<String, String> offeringSessions) {
+    public void setOfferingSessions(ArrayList<String> offeringSessions) {
         this.offeringSessions = offeringSessions;
     }
 
-    public void setPrereqs(HashMap<String, String> prereqs) {
+    public void setPrereqs(ArrayList<String> prereqs) {
         this.prereqs = prereqs;
     }
 
