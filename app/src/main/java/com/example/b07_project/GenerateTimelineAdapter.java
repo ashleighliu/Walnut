c7612fe9eece53ddc2b936695d19b073e713c54a
mirajismail <mirajismail.2003@gmail.com>
2022-12-04 23:46:26 -0500

    implement timeline algorithm

--- app/src/main/java/com/example/b07_project/GenerateTimelineAdapter.java
+++ app/src/main/java/com/example/b07_project/GenerateTimelineAdapter.java
@@ -1,5 +1,7 @@
 package com.example.b07_project;
 
+import static com.example.b07_project.courseTimelineList.coursesToTimeline;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
@@ -16,10 +18,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class GenerateTimelineAdapter extends RecyclerView.Adapter<GenerateTimelineAdapter.MyViewHolder> {
 
@@ -38,8 +45,15 @@ public class GenerateTimelineAdapter extends RecyclerView.Adapter<GenerateTimeli
     DatabaseReference allCourses;
     DatabaseReference userCoursesTaken;
     SharedPreferences p;
-    ArrayList<History> list;
 
+
+    ArrayList<String> historyCodes = new ArrayList<>();
+
+    ArrayList<GenerateTimeline> list;
+
+    HashMap<String, ArrayList<String>> sched = new HashMap<>();
+    HashMap<String, String> lastPre = new HashMap<>();
+    String followingCourse = "";
 
     //Courses = {courseCode: <{sessions:<courses>}, {prereqs:<courses>}>}
 
@@ -53,14 +67,14 @@ public class GenerateTimelineAdapter extends RecyclerView.Adapter<GenerateTimeli
 
     HashMap<String, ArrayList<String>> mapOfSessionPrereq = new HashMap<>();
 
-//    ArrayList<String> coursesUnderSession = new ArrayList<>();
-//    ArrayList<String> coursesUnderPrereq = new ArrayList<>();
+    //arraylist of history course ids
+    //
 
 
     ////////// REMEMBER TO FIX THIS /////////////
-    public GenerateTimelineAdapter(Context context, HashMap<String, String> session_courses) {
+    public GenerateTimelineAdapter(Context context, ArrayList<GenerateTimeline> list) {
         this.context = context;
-        //this.session_courses = session_courses;
+        this.list = list;
     }
 
     @NonNull
@@ -90,9 +104,8 @@ public class GenerateTimelineAdapter extends RecyclerView.Adapter<GenerateTimeli
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
-        //History user = list.get(position);
-        //holder.firstName.setText(user.getFirstName());
+        GenerateTimeline generateTimeline = list.get(position);
+        holder.sessionCourse.setText(generateTimeline.getSession() + ": " + generateTimeline.getCourses());
 
 
     }
@@ -104,6 +117,7 @@ public class GenerateTimelineAdapter extends RecyclerView.Adapter<GenerateTimeli
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView sessionCourse;
         OfferingSession offeringSession;
         ArrayList<String> offeringSessionsForCourses;
         ArrayList<String> prereqForCourses;
@@ -118,6 +132,8 @@ public class GenerateTimelineAdapter extends RecyclerView.Adapter<GenerateTimeli
         String courseCode;
         String courseCode2;
 
+        HashMap<String, ArrayList<String>> schedule = new HashMap<>();
+
 //        TextView firstName, lastName, age;
 
         public MyViewHolder(@NonNull View itemView) {
@@ -130,6 +146,11 @@ public class GenerateTimelineAdapter extends RecyclerView.Adapter<GenerateTimeli
                         addCourseID = courseID.getCourseID(); //actual value of courseID to be added somewhere
                         historyCourseCode = dataSnapshot.getValue(History.class);
                         courseCode = historyCourseCode.getCourseCode();
+
+                        if (history.contains(addCourseID)) {
+                            historyCodes.add(courseCode.toLowerCase());
+                        }
+
                         mapOfSessionPrereq = new HashMap<>();
                         Prereq prereq = dataSnapshot.getValue(Prereq.class);
                         prereqForCourses = new ArrayList<String>();
@@ -142,8 +163,8 @@ public class GenerateTimelineAdapter extends RecyclerView.Adapter<GenerateTimeli
                                 addCourseID2 = courseID2.getCourseID();
                                 historyCourseCode2 = preSnap.getValue(History.class);
                                 courseCode2 = historyCourseCode2.getCourseCode();
-                                if(pre.equals(courseID2)){
-                                    prereqForCourses.set(prereqForCourses.indexOf(pre), courseCode2);
+                                if(pre.equals(addCourseID2)){
+                                    prereqForCourses.set(prereqForCourses.indexOf(pre), courseCode2.toLowerCase());
                                 }
                             }
                         }
@@ -152,23 +173,11 @@ public class GenerateTimelineAdapter extends RecyclerView.Adapter<GenerateTimeli
                         offeringSession = dataSnapshot.getValue(OfferingSession.class);
                         offeringSessionsForCourses = new ArrayList<String>();
                         for (String s: offeringSession.getOfferingSessions()){
-                            offeringSessionsForCourses.add(s); //offeringSessionsForCourses contain vals of course sessions t.b.a
+                            offeringSessionsForCourses.add(s.toLowerCase()); //offeringSessionsForCourses contain vals of course sessions t.b.a
                         }
 
                         mapOfSessionPrereq.put("sessions", offeringSessionsForCourses);
                         allCoursesMap.put(courseCode, mapOfSessionPrereq);
-
-                        /// building mapallcourses
-                        //for pre in prereqforcourses:
-                        //      for each datasnapshot:
-                        //          courseID2 = datasnapshot...
-                        //          addCourseID2 = courseID2.getCourseID()
-                        //          historyCourseCode2 = datasnapshot...
-                        //          coursecode2 = historycoursecode2.getCourseCode()
-                        //          if pre = addCourseID2: prereq.set(prereqforcourses.indexof(pre), courseCode2);
-                        //mapOfSessionPrereq.put("prereqs", prereqForCourses);
-                        //do mapOfSessionPrereq.put("sessions", offeringSessionsForCourses)
-                        //allCoursesMap.put("courseCode", mapOfSessionPrereq) <-- Confirm if it's the actual courseCode or 'courseCode'
                     }
 
                 }
@@ -178,29 +187,212 @@ public class GenerateTimelineAdapter extends RecyclerView.Adapter<GenerateTimeli
 
                 }
             });
-//            firstName = itemView.findViewById(R.id.tvfirstName);
-//            lastName = itemView.findViewById(R.id.tvlastName);
-//            age = itemView.findViewById(R.id.tvage);
-//            allCoursesMap.put(courseCode,);
-//            mapOfSessionPrereq.put("sessions", offeringSessionsForCourses);
-//            mapOfSessionPrereq.put("prereqs", prereqForCourses);
-
-
-
+            schedule = buildSched(coursesToTimeline, historyCodes, sched, lastPre, followingCourse);
+            sessionCourse = itemView.findViewById(R.id.tvsessionCourse);
 
         }
     }
 
-    public HashMap buildSched(ArrayList wantToTake, ArrayList history,
+
+    //remember to convert history to lowercase
+    public HashMap buildSched(ArrayList<String> wantToTake, ArrayList<String> history,
                               HashMap<String, ArrayList<String>> sched,
                               HashMap<String, String> lastPre, String followingCourse) {
-        ArrayList<String> coursesToSched = new ArrayList<String>();
 
-        if (!(followingCourse.equals(""))) {
-
+        String[] date;
+        date = java.time.LocalDate.now().toString().split("-");
+        String currentSession;
+        if (Integer.parseInt(date[1]) >= 9 && Integer.parseInt(date[1])<=12) {
+            currentSession = "fall " + date[0];
         }
+        else if (Integer.parseInt(date[1]) >= 1 && Integer.parseInt(date[1])<=4){
+            currentSession = "winter " + date[0];
+        }
+        else{
+            currentSession = "summer " + date[0];
+        }
+        //basecase
+        if (wantToTake.size() == 1 && allCoursesMap.get(wantToTake.get(0)).get("prereqs").contains("null")) {
 
+            String nextOfferedSession;
+            nextOfferedSession = nextSession(wantToTake.get(0), currentSession);
+            history.add(wantToTake.get(0));
+            ArrayList<String> singleCourseList = new ArrayList<>(Arrays.asList(wantToTake.get(0)));
+            if (!sched.containsKey(nextOfferedSession)){
+                sched.put(nextOfferedSession, singleCourseList);
+            }
+            else{
+                ArrayList<String> coursesToPutInSched;
+                coursesToPutInSched = sched.get(nextOfferedSession);
+                coursesToPutInSched.add(wantToTake.get(0));
+                sched.put(nextOfferedSession, coursesToPutInSched);
+            }
 
+            if (!followingCourse.equals("")){
+                if (!lastPre.containsKey(followingCourse)) {
+                    lastPre.put(followingCourse, nextOfferedSession);
+                }
+                else{
+                    String current;
+                    current = lastPre.get(followingCourse);
+                    if (Integer.parseInt(nextOfferedSession.substring(nextOfferedSession.length() - 4))
+                            > Integer.parseInt(current.substring(current.length() - 4))) {
+                        lastPre.put(followingCourse, nextOfferedSession);
+                    } else if (Integer.parseInt(nextOfferedSession.substring(nextOfferedSession.length() - 4))
+                            == Integer.parseInt(current.substring(current.length() - 4))
+                            && (nextOfferedSession.substring(0, 4).equals("fall")) ||
+                            (nextOfferedSession.substring(0, 6).equals("summer") && (current.substring(0, 6).equals("winter")))) {
+                        lastPre.put(followingCourse, nextOfferedSession);
+                    }
+                }
+            }
+            return sched;
+        }
+        for (String course : wantToTake) {
+            if(!history.contains(course)) {
+                for (String pre: allCoursesMap.get(course).get("prereqs")){
+                    if (!history.contains(pre)){
+                        ArrayList<String> listOfSinglePre = new ArrayList<String>(Arrays.asList(pre));
+                        sched = buildSched(listOfSinglePre, history, sched, lastPre, course);
+                    }
+                    else{
+                        String current;
+                        if (lastPre.containsKey(course)){
+                            current = lastPre.get(course);
+                            if (Integer.parseInt(currentSession.substring(currentSession.length() - 4))
+                                    > Integer.parseInt(current.substring(current.length() - 4))) {
+                                lastPre.put(course, currentSession);
+                            } else if (Integer.parseInt(currentSession.substring(currentSession.length() - 4))
+                                    == Integer.parseInt(current.substring(current.length() - 4))
+                                    && (currentSession.substring(0, 4).equals("fall")) ||
+                                    (currentSession.substring(0, 6).equals("summer") && (current.substring(0, 6).equals("winter")))) {
+                                lastPre.put(course, currentSession);
+                            }
+                        }
+                        else{
+                            lastPre.put(course, currentSession);
+                        }
+                    }
+                }
+                String nextOfferedSession = nextSession(course, lastPre.get(course));
+                if (!history.contains(course)){
+                    ArrayList<String> singleCourseList = new ArrayList<>(Arrays.asList(course));
+                    //remember to append if key exists
+                    sched.put(nextOfferedSession, singleCourseList);
+                }
+                history.add(course);
+                if (!followingCourse.equals("")){
+                    if (!lastPre.containsKey(followingCourse)) {
+                        lastPre.put(followingCourse, nextOfferedSession);
+                    }
+                    else{
+                        String currentSess;
+                        currentSess = lastPre.get(followingCourse);
+                        if (Integer.parseInt(nextOfferedSession.substring(nextOfferedSession.length() - 4))
+                                > Integer.parseInt(currentSess.substring(currentSess.length() - 4))) {
+                            lastPre.put(followingCourse, nextOfferedSession);
+                        } else if (Integer.parseInt(nextOfferedSession.substring(nextOfferedSession.length() - 4))
+                                == Integer.parseInt(currentSess.substring(currentSess.length() - 4))
+                                && (nextOfferedSession.substring(0, 4).equals("fall")) ||
+                                (nextOfferedSession.substring(0, 6).equals("summer") && (currentSess.substring(0, 6).equals("winter")))) {
+                            lastPre.put(followingCourse, nextOfferedSession);
+                        }
+                    }
+                }
+            }
+        }
         return sched;
     }
+
+
+    //a -> b -> c -> d
+
+    public String earliestSession(String course) { // "session" + "year"
+        String[] date;
+        date = java.time.LocalDate.now().toString().split("-");
+        String year, month, day;
+        year = date[0];
+        month = date[1];
+        day = date[2];
+
+        if (Integer.parseInt(month) >= 9 && Integer.parseInt(month)<=12){ // if session = fall
+            if (allCoursesMap.get(course).get("sessions").contains("winter")){
+                return "winter " + Integer.toString(Integer.parseInt(year)+1);
+            }
+            else if(allCoursesMap.get(course).get("sessions").contains("summer")){
+                return "summer " + Integer.toString(Integer.parseInt(year)+1);
+            }
+            else{
+                return "fall " + Integer.toString(Integer.parseInt(year)+1);
+            }
+        }
+        else if (Integer.parseInt(month) >= 1 && Integer.parseInt(month)<= 4){ // if session == winter
+            if (allCoursesMap.get(course).get("sessions").contains("summer")){
+                return "summer " + year;
+            }
+            else if(allCoursesMap.get(course).get("sessions").contains("fall")){
+                return "fall " + year;
+            }
+            else{
+                return "winter " + Integer.toString(Integer.parseInt(year)+1);
+            }
+        }
+        else{ // if session is summer
+            if (allCoursesMap.get(course).get("sessions").contains("fall")){
+                return "fall " + year;
+            }
+            else if(allCoursesMap.get(course).get("sessions").contains("winter")){
+                return "winter " + Integer.toString(Integer.parseInt(year)+1);
+            }
+            else{
+                return "summer " + Integer.toString(Integer.parseInt(year)+1);
+            }
+        }
+    }
+    public String nextSession(String course, String sessionOfPre) {
+        String session = sessionOfPre.substring(0,sessionOfPre.length() - 4);
+        int year = Integer.parseInt(sessionOfPre.substring((sessionOfPre.length()-4)));
+
+        if (session.equals("fall")) {
+            if (allCoursesMap.get(course).get("sessions").contains("winter")){
+                return "winter " + Integer.toString(year+1);
+            }
+            else if(allCoursesMap.get(course).get("sessions").contains("summer")){
+                return "summer " + Integer.toString(year+1);
+            }
+            else{
+                return "fall " + Integer.toString(year+1);
+            }
+        }
+        else if(session.equals("winter")) {
+            if (allCoursesMap.get(course).get("sessions").contains("summer")){
+                return "summer " + year;
+            }
+            else if(allCoursesMap.get(course).get("sessions").contains("fall")){
+                return "fall " + year;
+            }
+            else{
+                return "winter " + Integer.toString(year+1);
+            }
+        }
+        else{
+            if (allCoursesMap.get(course).get("sessions").contains("fall")){
+                return "fall " + year;
+            }
+            else if(allCoursesMap.get(course).get("sessions").contains("winter")){
+                return "winter " + Integer.toString(year+1);
+            }
+            else{
+                return "summer " + Integer.toString(year+1);
+            }
+        }
+    }
 }
+
+
+//want to take
+
+
+
+
+
--- app/src/main/java/com/example/b07_project/Timeline.java
+++ app/src/main/java/com/example/b07_project/Timeline.java
@@ -1,6 +1,8 @@
 package com.example.b07_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +13,9 @@ import android.widget.Button;
 import java.util.ArrayList;
 
 public class Timeline extends AppCompatActivity {
-
+    RecyclerView recyclerView;
+    GenerateTimelineAdapter myAdapter;
+    ArrayList<GenerateTimeline> list;
     Button timelineBackButton;
 
     @Override
@@ -19,12 +23,17 @@ public class Timeline extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            ArrayList<String> timelineCourses = extras.getStringArrayList("key");
-            Log.d("Courses in Table", timelineCourses.toString());
-            //The key argument here must match that used in the other activity
-        }
+        recyclerView = findViewById(R.id.Timeline);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        myAdapter = new GenerateTimelineAdapter(this,list);
+        recyclerView.setAdapter(myAdapter);
+
+        // sched in buildSched in GenerateTimelineAdapter is our desired hashmap
+        // we want to take each key-value pair of sched and append it to list
+        // myAdapter.notifyDataSetChanged();
 
         timelineBackButton = findViewById(R.id.timelineBackButton);
         timelineBackButton.setOnClickListener(new View.OnClickListener() {
